ad07d2e34ea6ff50d1bbb78a86bd0f68f5f5efdd
mojahid369 <mdmojahidislam369@gmail.com>
2026-08-11 09:22:40 +0600

    Initial React setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <h2>i will learn react full course within 20 days</h2>
+      <p>this is my first react application</p>
+      <p>hello sala babu</p>
+
+    </div>
+  );
+}
+
+export default App;
