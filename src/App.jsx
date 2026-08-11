0ecea6d97473a810931e4e4aafc40d814dddee07
mojahid369 <mdmojahidislam369@gmail.com>
2026-08-11 19:52:54 +0600

    jsx learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
+import "./App.css";
 
 
 function App(){
+
+  const name="mojahid";
+  const age=24;
+
+  const fruits = ["Apple", "Banana", "Mango"];
+
+  let status;
+  if(age>18){
+    status="Aduilt";
+  }
+  else{
+    status="Minor";
+  }
+  
   return(
-    <div>
+   <div>
+        <h2>Name: {name}</h2>
+        <p>Age: {age}</p>
+        <h2>{status} </h2>
 
-      <Header/>
+        <ul>
+          {fruits.map((fruit)=> (
+              <li>{fruit} </li>
+          ))}
+        </ul>
+        
+        {age>18? <h2>Aduilt</h2> :<h2>Minor</h2> }
 
-      <About/> 
-
-      <Skills/>
-
-      <Projects/>
-
-      <Footer/>
-
-     
-    </div>
+        <div className="greet">
+          <h1>Hello Everyone</h1>
+        </div>
+   </div>
   )
 }
 
