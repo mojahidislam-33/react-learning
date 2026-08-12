125ec42cbed288b393219969edeedcf4783a6052
mojahid369 <mdmojahidislam369@gmail.com>
2026-08-12 16:07:22 +0600

    user login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,10 @@
-import { useState } from "react";
-function App() {
-  
-  function handleClick(event){
-     console.log("Button Click")
-  }
+import LoginForm from "./components/loginForm";
 
-  function handleChange(event){
-    console.log(event.target.value)
-
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log("Form Submited");
-  }
-
-  return (
+function App(){
+  return(
     <div>
-      <button onClick={handleClick}>
-        Clicked Me
-      </button>
-
-      <input type="text" onChange={handleChange} />
-
-      <form onSubmit={handleSubmit}>
-        <input type="text" />
-        <button type="submit">
-          Submit
-        </button>
-      </form>
+      <LoginForm />
     </div>
-
-    
   );
 }
 
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+function LoginForm(){
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+
+    function handleSubmit(event){
+        event.preventDefault();
+
+        if(email==="" || password === ""){
+            console.log("Please fill all fields")
+            return ;
+        }
+
+        console.log("Email:" ,email)
+        console.log("Password",password)
+    }
+
+    function handleChange(event){
+        console.log(event.target.value)
+    }
+
+    return(
+        <div>
+            <h2>Login Form</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input
+                      type="text" 
+                      value={email}
+                      onChange={(event)=>setEmail(event.target.value)}
+                      placeholder="enter email.."
+                     />
+                </div>
+
+                <br/>
+
+                <div>
+                    <label>Password:</label>
+                    <input 
+                    type="text" 
+                    value={password}
+                    onChange={(event)=>setPassword(event.target.value)}
+                    placeholder="enter password.."
+                    />
+                </div>
+
+                <br/>
+
+                <button type="submit">SUBMIT</button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
